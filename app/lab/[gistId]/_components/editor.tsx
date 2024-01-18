d2a6baf0fb202d5379871148238ed32c4aedf07d
goamaan <amaangokak18@gmail.com>
2024-01-18 03:19:18 -0800

    wip

--- app/lab/[gistId]/_components/editor.tsx
+++ app/lab/[gistId]/_components/editor.tsx
@@ -94,7 +94,7 @@ export const GistEditor = ({ gist }: { gist: Gist }) => {
 
     return (
         <div className="flex flex-col h-full w-full">
-            <nav className="pl-14 bg-primary/5 dark:bg-secondary/40 px-3 py-2 w-full flex items-center gap-x-4">
+            <nav className="pl-14 bg-primary/10 dark:bg-secondary/40 px-3 py-2 w-full flex items-center gap-x-4">
                 <div className="flex items-center justify-between w-full">
                     <h1>{`${gist.filename}.${
                         languageIdExtensionMap[gist.languageId]
